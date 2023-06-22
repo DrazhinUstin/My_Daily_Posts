9840c255b7447513d5f353eb352541a070b0730e
Drazhin Ustin <zmagar91@gmail.com>
2023-06-23 01:52:39 +0300

    Fix post deletion

--- src/components/PostCardMenu.jsx
+++ src/components/PostCardMenu.jsx
@@ -1,14 +1,51 @@
 import { useState } from 'react';
+import { toast } from 'react-toastify';
+import { doc, collection, getDocs, writeBatch, deleteDoc } from 'firebase/firestore';
+import { ref, deleteObject } from 'firebase/storage';
+import { db, storage } from '../firebase';
 import { usePostContext } from '../contexts/PostContext';
 import { Button, AlertButton } from '../styled';
 import { FaEllipsisH, FaEdit, FaTrashAlt } from 'react-icons/fa';
 
 const PostCardMenu = ({ post }) => {
+    const [isLoading, setIsLoading] = useState(false);
     const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const { dispatch, deletePost } = usePostContext();
+    const { dispatch } = usePostContext();
+
+    const deletePost = async () => {
+        setIsLoading(true);
+        try {
+            if (post.imageURL) {
+                const [res] = await Promise.allSettled([
+                    deleteObject(ref(storage, `posts/${post.id}`)),
+                ]);
+                if (res.status === 'rejected' && res.reason?.code !== 'storage/object-not-found') {
+                    throw Error(res.reason?.message || 'There was an error');
+                }
+            }
+            const { docs } = await getDocs(collection(db, `posts/${post.id}/comments`));
+            const chunks = [];
+            for (let i = 0; i < docs.length; i += 500) {
+                chunks.push(docs.slice(i, i + 500));
+            }
+            await Promise.all(
+                chunks.map(async (chunk) => {
+                    const batch = writeBatch(db);
+                    chunk.forEach((doc) => batch.delete(doc.ref));
+                    return batch.commit();
+                })
+            );
+            await deleteDoc(doc(db, `posts/${post.id}`));
+            toast.success('Post was deleted!');
+        } catch (error) {
+            toast.error(error.message);
+        }
+        setIsLoading(false);
+    };
+
     return (
         <div className='controls'>
-            <Button $icon onClick={() => setIsMenuOpen(!isMenuOpen)}>
+            <Button $icon onClick={() => setIsMenuOpen(!isMenuOpen)} disabled={isLoading}>
                 <FaEllipsisH />
             </Button>
             {isMenuOpen && (
@@ -19,7 +56,7 @@ const PostCardMenu = ({ post }) => {
                     >
                         <FaEdit /> edit
                     </Button>
-                    <AlertButton $flex onClick={() => deletePost(post)}>
+                    <AlertButton $flex onClick={deletePost}>
                         <FaTrashAlt /> delete
                     </AlertButton>
                 </div>
--- src/contexts/PostContext.jsx
+++ src/contexts/PostContext.jsx
@@ -1,9 +1,5 @@
 import { createContext, useContext, useReducer } from 'react';
-import { doc, deleteDoc } from 'firebase/firestore';
-import { ref, deleteObject } from 'firebase/storage';
-import { db, storage } from '../firebase';
 import reducer from '../reducers/postReducer';
-import { toast } from 'react-toastify';
 
 const PostContext = createContext();
 export const usePostContext = () => useContext(PostContext);
@@ -15,22 +11,7 @@ const initialState = {
 
 const PostProvider = ({ children }) => {
     const [state, dispatch] = useReducer(reducer, initialState);
-
-    const deletePost = async (post) => {
-        try {
-            await deleteDoc(doc(db, `posts/${post.id}`));
-            if (post.imageURL) await deleteObject(ref(storage, `posts/${post.id}`));
-            toast.success('Post was deleted!');
-        } catch (error) {
-            toast.error(error.message);
-        }
-    };
-
-    return (
-        <PostContext.Provider value={{ ...state, dispatch, deletePost }}>
-            {children}
-        </PostContext.Provider>
-    );
+    return <PostContext.Provider value={{ ...state, dispatch }}>{children}</PostContext.Provider>;
 };
 
 export default PostProvider;
